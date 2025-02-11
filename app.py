3c0cf961598a9e88b4d32d43cd7ff1ba4f0e10f7
AbderrahimRezki <abderrahim4cem@gmail.com>
2025-02-11 11:29:10 +0100

    added mine block

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from blockchain import Blockchain
+import json
+
+app = Flask(__name__)
+blockchain = Blockchain()
+
+@app.route("/mine_block", methods=["GET"])
+def mine_block():
+    prev_block = blockchain.print_previous_block()
+    
+    prev_proof = prev_block['proof']
+    new_proof = Blockchain.proof_of_work(previous_proof=prev_proof)
+    
+    prev_hash = Blockchain.hash(prev_block)
+    new_block = blockchain.create_block(new_proof, prev_hash)
+    
+    response = json.dump({
+        "message": "Block mined successfully.",
+        **new_block
+    })
+    
+    return response, 200
--- blockchain.py
+++ blockchain.py
@@ -21,11 +21,12 @@ class Blockchain:
     def print_previous_block(self):
         return self.chain[-1]
     
-    def proof_of_work(self, previous_proof):
+    @staticmethod
+    def proof_of_work(previous_proof):
         new_proof = 1
         
         while True:
-            if self.__validate_proof(previous_proof, new_proof):
+            if Blockchain.__validate_proof(previous_proof, new_proof):
                 break
             
             new_proof += 1
@@ -33,7 +34,7 @@ class Blockchain:
         return new_proof
     
     @staticmethod
-    def hash(self, block):
+    def hash(block):
         sha256 = hashlib.sha256()        
         json_block = json.dumps(block, sort_keys=True).encode()
         sha256.update(json_block)
@@ -41,16 +42,16 @@ class Blockchain:
         return sha256.hexdigest()
     
     @staticmethod
-    def chain_valid(self, chain):
+    def chain_valid(chain):
         genesis = chain[0]
         
-        if genesis != self.chain[0]:
+        if genesis['index'] != 0 or genesis['proof'] != 1 or genesis['previous_hash'] != '0':
             return False
         
         for i in range(1, len(chain) - 1):
             curr, prev = chain[i], chain[i-1]
             
-            actual_prev_hash = self.hash(prev)
+            actual_prev_hash = Blockchain.hash(prev)
             prev_hash = curr["previous_hash"]
             
             if prev_hash != actual_prev_hash:
@@ -59,13 +60,13 @@ class Blockchain:
             prev_proof = prev["proof"]
             proof = curr['proof']
             
-            if not self.__validate_proof(prev_proof, proof):
+            if not Blockchain.__validate_proof(prev_proof, proof):
                 return False
             
         return True
     
     @staticmethod
-    def __validate_proof(self, previous_proof, new_proof):
+    def __validate_proof(previous_proof, new_proof):
         sha256 = hashlib.sha256()
         sha256.update(str(previous_proof**2 - new_proof**2).encode())
         hash_value = sha256.hexdigest()
